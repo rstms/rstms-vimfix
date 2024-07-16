d5f9c2d8d68a36b0390f1b925f920aab88ca2488
Matt Krueger <mkrueger@rstms.net>
2024-07-15 20:38:47 -0600

    add vimfix logic; update cli config; replace boilerplate with vimfix function call

--- rstms_vimfix/cli.py
+++ rstms_vimfix/cli.py
@@ -8,6 +8,7 @@ import click.core
 
 from .version import __version__, __timestamp__
 from .exception_handler import ExceptionHandler
+from .vimfix import vimfix, formats
 
 
 from .shell import _shell_completion
@@ -20,30 +21,29 @@ def _ehandler(ctx, option, debug):
     ctx.obj["debug"] = debug
 
 
-
-@click.group("vimfix", context_settings={"auto_envvar_prefix": "VIMFIX"})
+@click.command("vimfix", context_settings={"auto_envvar_prefix": "VIMFIX"})
 @click.version_option(message=header)
 @click.option("-d", "--debug", is_eager=True, is_flag=True, callback=_ehandler, help="debug mode")
 @click.option("--shell-completion", is_flag=False, flag_value="[auto]", callback=_shell_completion, help="configure shell completion")
-@click.pass_context
-def cli(ctx, debug, shell_completion):
-    """Run a compile command and process the output for vim quickfix mode"""
-    pass
+@click.option("-c", "--command", help="command to run", required=True)
+@click.option("-q", "--quiet", is_flag=True, help="no echo stdout")
+@click.option("-E", "--ignore-stderr", is_flag=True, help="ignore stderr when scanning")
+@click.option("-O", "--ignore-stdout", is_flag=True, help="ignore stdout when scanning")
+@click.option(
+    "-s/-S", "--strip/--no-strip", is_flag=True, default=True, help="strip ANSI codes"
+)
+@click.option(
+    "-f",
+    "--format", "fmt",
+    type=click.Choice(list(formats.keys())),
+    default=list(formats.keys())[0],
+)
+@click.option(
+    "-o", "--output", type=click.Path(dir_okay=False, writable=True, path_type=Path)
+)
 
-@cli.command
-@click.option("-r", "--raises", type=str, show_envvar=True, help='example option')
-@click.option("-f", "--flag", is_flag=True, help='example flag option')
-@click.option("-i", "--input-file", type=click.Path(dir_okay=False, readable=True, exists=True, path_type=Path), help="input file")
-@click.option("-o", "--output-file", type=click.Path(dir_okay=False, writable=True, exists=False, path_type=Path), help="output file")
-@click.argument('input', type=click.File('r'))
-@click.argument('output', type=click.File('w'), required=False, default='-')
-@click.pass_context
-def action(ctx, raises, flag, input_file, output_file, input, output):
-    """action command help"""
-
-    if raises == "exception":
-        raise RuntimeError(raises)
-
+def cli(ctx, debug, shell_completion, command, quiet, ignore_stderr, ignore_stdout, strip, fmt, output):
+    quickfix(command, quiet, ignore_stderr, ignore_stdout, strip, fmt, output)
 
 if __name__ == "__main__":
     sys.exit(cli())  # pragma: no cover
