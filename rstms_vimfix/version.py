f7c9d4a79782c4db7f108c5d113f802b970e1b00
Matt Krueger <mkrueger@rstms.net>
2024-07-31 13:42:41 -0600

    Bump version: 0.1.15 → 0.1.16

--- rstms_vimfix/version.py
+++ rstms_vimfix/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2024-07-16 02:02:35-06:00"
+__timestamp__ = "2024-07-31 13:42:41-06:00"
